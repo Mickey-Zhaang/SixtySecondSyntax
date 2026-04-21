faafd7d9c9680c918d1043d53f40350f7d0da666
Mickey-Zhaang <mickey.zhaang@gmail.com>
2026-04-21 08:48:31 -0400

    modified soy coke glazed chicken

--- src/content/articles/FOOD/soy-coke-glazed-chicken.tsx
+++ src/content/articles/FOOD/soy-coke-glazed-chicken.tsx
@@ -23,8 +23,8 @@ export const content = `
 ## Instructions
 1) Cut chicken thighs into small, bite-sized chunks
 2) In large bowl, toss in the chicken and add soy sauce oyster sauce, black pepper, garlic and let marinate for 5 minutes
-3) In larger pan, heat neutral oil on medium-high heat. Add in the chicken and pan-fry for 4-5 minutes or until it gets nice and brown.
-4) Pour in a can of Coke. Cover and let cook for 7-8 more minutes
+3) In larger pan, heat neutral oil on medium-high heat until hot. Add in the chicken and pan-fry for 4 minutes or until it gets nice and brown (make sure to not overcook).
+4) Pour in a can of Coke. Cover and let cook for 6-7 more minutes
 5) Once time is up, uncover and stir to ensure maximal glazing of chicken
 6) Serve over rice + veggies and enjoy!
 
